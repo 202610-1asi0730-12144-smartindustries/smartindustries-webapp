c402ea6ea42e285a52fc31f82c6fe49116bc8177
ImaLi06 <imanollimache@gmail.com>
2026-06-05 01:02:20 -0500

    feat: create search-bar component

---
+++ src/shared/presentation/components/search-bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="search-bar">
+    <pv-icon-field icon-position="left">
+      <pv-input-icon>
+        <i class="pi pi-search" />
+      </pv-input-icon>
+      <pv-input-text placeholder="Search..." />
+    </pv-icon-field>
+  </div>
+</template>
+
+<style scoped>
+.search-bar {
+  width: 100%;
+  max-width: 400px;
+}
+.search-bar :deep(.p-inputtext) {
+  border-radius: 999px;
+  background: #f5f5f5;
+}
+</style>
