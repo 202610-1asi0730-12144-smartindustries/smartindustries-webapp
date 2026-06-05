d7f080452c2ca6cb011f900baf433cede976053e
ImaLi06 <imanollimache@gmail.com>
2026-06-05 01:49:52 -0500

    feat(sites): add sites table

--- src/space-management/presentation/views/sites.vue
+++ src/space-management/presentation/views/sites.vue
@@ -1,5 +1,13 @@
 <script setup>
 import searchBar from "../../../shared/presentation/components/search-bar.vue";
+import { ref } from "vue"
+
+const sites = [
+  { id: 1, name: 'Building A' },
+  { id: 2, name: 'Building B' },
+  { id: 3, name: 'Warehouse' },
+  { id: 4, name: 'Remote' },
+]
 </script>
 
 <template>
@@ -8,6 +16,15 @@ import searchBar from "../../../shared/presentation/components/search-bar.vue";
     <div class="filter-bar">
       <search-bar/>
     </div>
+    <pv-data-table :value="sites" stripedRows style="width: 100%">
+      <pv-column field="id" header="ID" />
+      <pv-column field="name" header="Name" />
+      <pv-column header="" style="width: 4rem">
+        <template #body>
+          <pv-button icon="pi pi-ellipsis-v" rounded text plain />
+        </template>
+      </pv-column>
+    </pv-data-table>
   </div>
 </template>
 
@@ -20,5 +37,9 @@ import searchBar from "../../../shared/presentation/components/search-bar.vue";
   align-items: center;
   gap: 2rem;
   margin-bottom: 1rem;
+}
+.sites-view :deep(.p-datatable) {
+  width: 100%;
+  margin-top: 1rem;
 }
 </style>
