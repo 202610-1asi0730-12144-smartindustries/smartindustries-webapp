c44ff1194fa68bc4b318188bcc6d1ea5dbedfba3
ImaLi06 <imanollimache@gmail.com>
2026-06-05 13:35:33 -0500

    feat(roles): add filter bar

--- src/administration/presentation/views/roles.vue
+++ src/administration/presentation/views/roles.vue
@@ -1,11 +1,24 @@
 <script setup>
-
+import searchBar from "../../../shared/presentation/components/search-bar.vue";
 </script>
 
 <template>
-  <h1>Roles</h1>
+  <div class="roles-view">
+    <h1>Roles</h1>
+    <div class="filter-bar">
+      <search-bar/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.roles-view {
+  padding: 0.5rem 3rem;
+}
+.filter-bar {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  margin-bottom: 1rem;
+}
 </style>
