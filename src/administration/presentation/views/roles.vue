260b90af3f70f3c0a4f3015c28be5c07fccabe74
ImaLi06 <imanollimache@gmail.com>
2026-06-05 13:36:44 -0500

    feat(roles): add roles table

--- src/administration/presentation/views/roles.vue
+++ src/administration/presentation/views/roles.vue
@@ -1,5 +1,12 @@
 <script setup>
 import searchBar from "../../../shared/presentation/components/search-bar.vue";
+
+const roles = [
+  { id: 1, name: 'Super Admin' },
+  { id: 2, name: 'Admin' },
+  { id: 3, name: 'Manager' },
+  { id: 4, name: 'Operator' },
+]
 </script>
 
 <template>
@@ -8,6 +15,15 @@ import searchBar from "../../../shared/presentation/components/search-bar.vue";
     <div class="filter-bar">
       <search-bar/>
     </div>
+    <pv-data-table :value="roles" stripedRows style="width: 100%">
+      <pv-column field="id" header="ID" />
+      <pv-column field="name" header="Name" />
+      <pv-column header="" style="width: 4rem">
+        <template #body>
+          <pv-button icon="pi pi-ellipsis-v" rounded text plain />
+        </template>
+      </pv-column>
+    </pv-data-table>
   </div>
 </template>
 
@@ -20,5 +36,9 @@ import searchBar from "../../../shared/presentation/components/search-bar.vue";
   align-items: center;
   gap: 2rem;
   margin-bottom: 1rem;
+}
+.roles-view :deep(.p-datatable) {
+  width: 100%;
+  margin-top: 1rem;
 }
 </style>
