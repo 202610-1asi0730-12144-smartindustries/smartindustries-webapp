901050bdd2451025308aa56a159274086ac0394d
ImaLi06 <imanollimache@gmail.com>
2026-06-05 23:55:37 -0500

    feat(administration): add roles to store

--- src/administration/application/administration.store.js
+++ src/administration/application/administration.store.js
@@ -2,12 +2,16 @@ import {defineStore} from "pinia";
 import {AdministrationApi} from "../infrastructure/administration-api.js";
 import {ref} from "vue";
 import {AdministratorAssembler} from "../infrastructure/administrator.assembler.js";
+import {RoleAssembler} from "../infrastructure/role.assembler.js";
 
 const administrationApi = new AdministrationApi();
 
 const useAdministrationStore = defineStore('administration', () => {
     const administrators = ref([]);
     const administratorsLoaded = ref(false);
+
+    const roles = ref([]);
+    const rolesLoaded = ref(false);
 
     const errors = ref([]);
 
@@ -21,11 +25,24 @@ const useAdministrationStore = defineStore('administration', () => {
         });
     }
 
+    function fetchRoles() {
+        administrationApi.getRoles().then(response => {
+            roles.value = RoleAssembler.toEntitiesFromResponse(response);
+            rolesLoaded.value = true;
+            console.log(rolesLoaded.value);
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
     return {
         administrators,
         administratorsLoaded,
+        roles,
+        rolesLoaded,
         errors,
         fetchAdministrators,
+        fetchRoles,
     }
 })
 
