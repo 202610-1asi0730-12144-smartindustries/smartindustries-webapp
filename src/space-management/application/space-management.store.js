6b977f0380f4b3151083e5dace961dec11161496
ImaLi06 <imanollimache@gmail.com>
2026-06-05 19:14:13 -0500

    feat(space-management): add orgnizations to store

--- src/space-management/application/space-management.store.js
+++ src/space-management/application/space-management.store.js
@@ -1,10 +1,28 @@
 import {defineStore} from "pinia";
+import {SpaceManagementApi} from "../infrastructure/space-management-api.js";
+import {ref} from "vue";
+import {OrganizationAssembler} from "../infrastructure/organization.assembler.js";
 
-const useSpaceManagementStore = defineStore(
-    'space-management',
-    () => {
-        
+const spaceManagementApi = new SpaceManagementApi();
 
-        return {
-        }
+const useSpaceManagementStore = defineStore('space-management', () => {
+    const organizations = ref([]);
+    const organizationsLoaded = ref(false);
+
+    const errors = ref([]);
+
+    function fetchOrganizations() {
+        spaceManagementApi.getOrganizations().then(response => {
+            organizations.value = OrganizationAssembler.toEntitiesFromResponse(response);
+            organizationsLoaded.value = true;
+            console.log(organizationsLoaded.value);
+            console.log(organizationsLoaded.value);
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+    return {
+        organizations
+    }
 })
