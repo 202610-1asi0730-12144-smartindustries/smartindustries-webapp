dc5a3606169a648c9674e42362a470598be5c8ca
ImaLi06 <imanollimache@gmail.com>
2026-06-05 23:54:14 -0500

    feat(administration): add rolesEndpoint to api

--- src/administration/infrastructure/administration-api.js
+++ src/administration/infrastructure/administration-api.js
@@ -2,16 +2,23 @@ import {BaseApi} from "../../shared/infrastructure/base-api.js";
 import {BaseEndpoint} from "../../shared/infrastructure/base-endpoint.js";
 
 const administratorsEndpointPath = import.meta.env.VITE_ADMINISTRATORS_ENDPOINT_PATH;
+const rolesEndpointPath = import.meta.env.VITE_ROLES_ENDPOINT_PATH;
 
 export class AdministrationApi extends BaseApi {
     #administratorsEndpoint;
+    #rolesEndpoint;
 
     constructor() {
         super();
         this.#administratorsEndpoint = new BaseEndpoint(this, administratorsEndpointPath);
+        this.#rolesEndpoint = new BaseEndpoint(this, rolesEndpointPath);
     }
 
     getAdministrators() {
         return this.#administratorsEndpoint.getAll();
+    }
+
+    getRoles() {
+        return this.#rolesEndpoint.getAll();
     }
 }
