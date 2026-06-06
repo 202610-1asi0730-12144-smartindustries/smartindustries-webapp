3bdcaabd7d5d5560bf0be7cd2b7d90070d63b08a
ImaLi06 <imanollimache@gmail.com>
2026-06-05 22:37:28 -0500

    feat(space-management): add sitesEndpoint to api

--- src/space-management/infrastructure/space-management-api.js
+++ src/space-management/infrastructure/space-management-api.js
@@ -4,17 +4,20 @@ import {BaseEndpoint} from "../../shared/infrastructure/base-endpoint.js";
 const organizationsEndpointPath = import.meta.env.VITE_ORGANIZATIONS_ENDPOINT_PATH;
 const peopleEndpointPath = import.meta.env.VITE_PEOPLE_ENDPOINT_PATH;
 const devicesEndpointPath = import.meta.env.VITE_DEVICES_ENDPOINT_PATH;
+const sitesEndpointPath = import.meta.env.VITE_SITES_ENDPOINT_PATH;
 
 export class SpaceManagementApi extends BaseApi {
     #organizationsEndpoint;
     #peopleEndpoint;
     #devicesEndpoint;
+    #sitesEndpoint;
 
     constructor() {
         super();
         this.#organizationsEndpoint = new BaseEndpoint(this, organizationsEndpointPath);
         this.#peopleEndpoint = new BaseEndpoint(this, peopleEndpointPath);
         this.#devicesEndpoint = new BaseEndpoint(this, devicesEndpointPath);
+        this.#sitesEndpoint = new BaseEndpoint(this, sitesEndpointPath);
     }
 
     getOrganizations() {
@@ -27,5 +30,9 @@ export class SpaceManagementApi extends BaseApi {
 
     getDevices() {
         return this.#devicesEndpoint.getAll();
+    }
+
+    getSites() {
+        return this.#sitesEndpoint.getAll();
     }
 }
